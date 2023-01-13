915fd424bc364ecc2b0d2f33cc6d313fdcf00512
jjobe5989 <s3thjobe@gmail.com>
2023-01-12 21:43:44 -0600

    fixed errors

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
+
 USE employees_db;
 
 CREATE TABLE employees (
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "7464533672907kJ?",
+  password: "",
   database: "employees_db"
 });
 
